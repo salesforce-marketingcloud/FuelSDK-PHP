be840a972e9efd73ebca7178aeb56f3b79d60ca4
Sharif Ahmed <sharif.ahmed@salesforce.com>
2017-10-20 11:15:02 -0400

    autoload without composer

--- objsamples/sample-bounceevent.php
+++ objsamples/sample-bounceevent.php
@@ -1,5 +1,6 @@
 <?php
-include_once('tests/UnitBootstrap.php');
+//include_once('tests/UnitBootstrap.php');
+include_once(__DIR__ . '/../src/autoload.php');
 use FuelSdk\ET_Client;
 use FuelSdk\ET_BounceEvent;
 
---
+++ src/autoload.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+spl_autoload_register( function($class_name) {
+    if (file_exists('src/'.$class_name.'.php'))
+        include_once 'src/'.$class_name.'.php';
+    else
+        include_once 'tests/'.$class_name.'.php';
+});
+date_default_timezone_set('UTC');
