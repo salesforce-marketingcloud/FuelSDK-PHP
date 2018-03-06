2625d854aec32ff6efafbc5ffa22054c3133fe83
Justin Foell <jrfoell@users.noreply.github.com>
2018-03-06 08:08:56 -0600

    Fix config.php include

--- src/ET_Client.php
+++ src/ET_Client.php
@@ -88,8 +88,9 @@ class ET_Client extends SoapClient
 
 		$this->xmlLoc = 'ExactTargetWSDL.xml';
 
-		if (file_exists(realpath("config.php")))
-			$config = include 'config.php';
+		$config_path = realpath(__DIR__ . "/../config.php");
+		if (file_exists($config_path))
+			$config = include $config_path;
 
 		if ($config)
 		{
