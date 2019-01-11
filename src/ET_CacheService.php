594ccc4442cc1fbcb6292756c518cf836a864c55
Dragos Rogojan <drogojan@salesforce.com>
2019-01-11 17:51:52 +0200

    ET_CacheService refactor and tests.

--- src/ET_CacheService.php
+++ src/ET_CacheService.php
@@ -2,92 +2,36 @@
 
 namespace FuelSdk;
 
-use FuelSdk\shmSmart;
-
-
 class ET_CacheService
 {
-
     private $_identifier;
-    private $_filePath = '../.cache';
     private $_cacheMinutes = 10;
-    private $shmCache;
+    private static $cachedSoapUrls;
 
     public function __construct($clientId, $clientSecret)
     {
         $this->_identifier = $clientId . "-" . $clientSecret;
-        $this->shmCache = new shmSmart();
     }
 
     public function get()
     {
-        $cachedValue = $this->shmCache->get($this->_identifier);
-        if($cachedValue == false) {
+        $now = time();
+        $data = ET_CacheService::$cachedSoapUrls[$this->_identifier];
+        if (!$data || !$data->expires || $data->expires < $now) {
+            // remove expired data from the array
+            unset(ET_CacheService::$cachedSoapUrls[$this->_identifier]);
             return null;
+        } else {
+            return $data;
         }
-        else {
-            return $cachedValue;
-        }
-
-//        $cache = $this->_getOrCreateFile();
-//        $data = $cache->{$this->_identifier};
-//        $now = time();
-//        if (!$data || !$data->expires || $data->expires < $now) {
-//            return null;
-//        } else {
-//            return $data;
-//        }
     }
 
     public function write($url)
     {
         $expires = time() + $this->_cacheMinutes * 60;
-        //$cache = $this->_getOrCreateFile();
-        $cache = null;
-
-        $cachedValue = $this->shmCache->get($this->_identifier);
-        if($cachedValue == false) {
-            $cache = new \stdClass();
-        }
-        else {
-            $cache = $cachedValue;
-        }
-
         $data = new \stdClass();
         $data->expires = $expires;
         $data->url = $url;
-        $cache->{$this->_identifier} = $data;
-        //$this->_writeFile($cache);
-
-        $this->shmCache->put($this->_identifier, $cache);
+        ET_CacheService::$cachedSoapUrls[$this->_identifier] = $data;
     }
-
-    public function clear()
-    {
-        $cache = $this->_getOrCreateFile();
-        unset($cache->{$this->_identifier});
-        $this->_writeFile($cache);
-    }
-
-    private function _getOrCreateFile()
-    {
-        if (file_exists($this->_filePath)) {
-            return $this->_readFile();
-        } else {
-            $data = new \stdClass();
-            $this->_writeFile($data);
-            return $data;
-        }
-    }
-
-    private function _writeFile($contents)
-    {
-        file_put_contents($this->_filePath, json_encode($contents));
-    }
-
-    private function _readFile()
-    {
-        return json_decode(file_get_contents($this->_filePath));
-    }
-
 }
