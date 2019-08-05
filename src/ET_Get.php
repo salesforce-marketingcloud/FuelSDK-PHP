61cb586dd73d1a944602cdf2317542d121e87997
Stan Alachniewicz <stan_alach@hotmail.com>
2019-08-05 11:25:18 -0400

    updated ET_Get.php to work with three filters

--- src/ET_Get.php
+++ src/ET_Get.php
@@ -10,100 +10,147 @@ use \SoapVar;
  */
 class ET_Get extends ET_Constructor
 {
-	/** 
-	* Initializes a new instance of the class.
-	* @param 	ET_Client   $authStub 	The ET client object which performs the auth token, refresh token using clientID clientSecret
-	* @param    string      $objType 	Object name, e.g. "ImportDefinition", "DataExtension", etc
-	* @param 	array       $props 		Dictionary type array which may hold e.g. array('id' => '', 'key' => '')
-	* @param 	array    	$filter 	Dictionary type array which may hold e.g. array("Property"=>"", "SimpleOperator"=>"","Value"=>"")
-	* @param 	bool		$getSinceLastBatch 	Gets or sets a boolean value indicating whether to get since last batch. true if get since last batch; otherwise, false.
-	*/	
-	function __construct($authStub, $objType, $props, $filter, $getSinceLastBatch = false)
-	{
-		$authStub->refreshToken();
-		$rrm = array();
-		$request = array();
-		$retrieveRequest = array();
-		
-		// If Props is not sent then Info will be used to find all retrievable properties
-		if (is_null($props)){	
-			$props = array();
-			$info = new ET_Info($authStub, $objType);
-			if (is_array($info->results)){	
-				foreach ($info->results as $property){	
-					if($property->IsRetrievable){	
-						$props[] = $property->Name;
-					}
-				}	
-			}
-		}
-		
-		if (ET_Util::isAssoc($props)){
-			$retrieveProps = array();
-			foreach ($props as $key => $value){	
-				if (!is_array($value))
-				{
-					$retrieveProps[] = $key;
-				}
-				$retrieveRequest["Properties"] = $retrieveProps;
-			}
-		} else {
-			$retrieveRequest["Properties"] = $props;	
-		}
-		
+  /** 
+  * Initializes a new instance of the class.
+  * @param  ET_Client   $authStub   The ET client object which performs the auth token, refresh token using clientID clientSecret
+  * @param    string      $objType  Object name, e.g. "ImportDefinition", "DataExtension", etc
+  * @param  array       $props    Dictionary type array which may hold e.g. array('id' => '', 'key' => '')
+  * @param  array     $filter   Dictionary type array which may hold e.g. array("Property"=>"", "SimpleOperator"=>"","Value"=>"")
+  * @param  bool    $getSinceLastBatch  Gets or sets a boolean value indicating whether to get since last batch. true if get since last batch; otherwise, false.
+  */  
+  function __construct($authStub, $objType, $props, $filter, $getSinceLastBatch = false)
+  {
+    $authStub->refreshToken();
+    $rrm = array();
+    $request = array();
+    $retrieveRequest = array();
     
-		$retrieveRequest["ObjectType"] = $objType;
-		//if ("Account" == $objType) {
-			$retrieveRequest["QueryAllAccounts"] = true;
-		//}
-		if ($filter){
-			if (array_key_exists("LogicalOperator",$filter )){				
-				$cfp = new stdClass();
-				$cfp->LeftOperand = new SoapVar($filter["LeftOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
-				$cfp->RightOperand = new SoapVar($filter["RightOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");				
-				$cfp->LogicalOperator = $filter["LogicalOperator"];
-				$retrieveRequest["Filter"] = new SoapVar($cfp, SOAP_ENC_OBJECT, 'ComplexFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
-				
-			} else {
-				$retrieveRequest["Filter"] = new SoapVar($filter, SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
-			}
-		}
-		if ($getSinceLastBatch) {
-			$retrieveRequest["RetrieveAllSinceLastBatch"] = true;
-		}
-		
-		
-		$request["RetrieveRequest"] = $retrieveRequest;
-		$rrm["RetrieveRequestMsg"] = $request;
-		
-		$return = $authStub->__soapCall("Retrieve", $rrm, null, null , $out_header);
-		parent::__construct($return, $authStub->__getLastResponseHTTPCode());
-		
-		if ($this->status){
-			if (property_exists($return, "Results")){
-				// We always want the results property when doing a retrieve to be an array
-				if (is_array($return->Results)){
-					$this->results = $return->Results;
-				} else {
-					$this->results = array($return->Results);
-				}
-			} else {
-				$this->results = array();
-			}
-			if ($return->OverallStatus != "OK" && $return->OverallStatus != "MoreDataAvailable")
-			{
-				$this->status = false;
-				$this->message = $return->OverallStatus;
-			}
+    // If Props is not sent then Info will be used to find all retrievable properties
+    if (is_null($props)){ 
+      $props = array();
+      $info = new ET_Info($authStub, $objType);
+      if (is_array($info->results)){  
+        foreach ($info->results as $property){  
+          if($property->IsRetrievable){ 
+            $props[] = $property->Name;
+          }
+        } 
+      }
+    }
+    
+    if (ET_Util::isAssoc($props)){
+      $retrieveProps = array();
+      foreach ($props as $key => $value){ 
+        if (!is_array($value))
+        {
+          $retrieveProps[] = $key;
+        }
+        $retrieveRequest["Properties"] = $retrieveProps;
+      }
+    } else {
+      $retrieveRequest["Properties"] = $props;  
+    }
+    
+    
+    $retrieveRequest["ObjectType"] = $objType;
+    //if ("Account" == $objType) {
+      $retrieveRequest["QueryAllAccounts"] = true;
+    //}
+    if ($filter){
 
-			$this->moreResults = false;
-			
-			if ($return->OverallStatus == "MoreDataAvailable") {				
-				$this->moreResults = true;
-			}
-				
-			$this->request_id = $return->RequestID;
-		}	
-	}
+
+      if (array_key_exists("LogicalOperator",$filter )){
+
+        function array_depth(array $array) {
+            $max_depth = 1;
+
+            foreach ($array as $value) {
+                if (is_array($value)) {
+                    $depth = array_depth($value) + 1;
+
+                    if ($depth > $max_depth) {
+                        $max_depth = $depth;
+                    }
+                }
+            }
+
+            return $max_depth;
+        }
+
+        $filterCount = array_depth($filter);
+        if( $filterCount == 2 ){
+          $cfp = new stdClass();
+          $cfp->LeftOperand = new SoapVar($filter["LeftOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+          $cfp->RightOperand = new SoapVar($filter["RightOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");       
+          $cfp->LogicalOperator = $filter["LogicalOperator"];
+          $retrieveRequest["Filter"] = new SoapVar($cfp, SOAP_ENC_OBJECT, 'ComplexFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+
+        }elseif( $filterCount == 3 ){
+
+
+          $left = new stdClass();
+          $left->LeftOperand = new SoapVar($filter["LeftOperand"]["LeftOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+          $left->RightOperand = new SoapVar($filter["LeftOperand"]["RightOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+          $left->LogicalOperator = $filter["LeftOperand"]["LogicalOperator"];          
+
+
+          $cfp = new stdClass();
+          $cfp->LeftOperand = new SoapVar($left, SOAP_ENC_OBJECT, 'ComplexFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+          $cfp->RightOperand = new SoapVar($filter["RightOperand"], SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");       
+          $cfp->LogicalOperator = $filter["LogicalOperator"];
+
+
+          $retrieveRequest["Filter"] = new SoapVar($cfp, SOAP_ENC_OBJECT, 'ComplexFilterPart', "http://exacttarget.com/wsdl/partnerAPI");     
+               
+        }
+
+
+
+      } else {
+        $retrieveRequest["Filter"] = new SoapVar($filter, SOAP_ENC_OBJECT, 'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+      }
+
+
+
+
+
+    }
+    if ($getSinceLastBatch) {
+      $retrieveRequest["RetrieveAllSinceLastBatch"] = true;
+    }
+    
+    
+    $request["RetrieveRequest"] = $retrieveRequest;
+    $rrm["RetrieveRequestMsg"] = $request;
+    
+    $return = $authStub->__soapCall("Retrieve", $rrm, null, null , $out_header);
+    parent::__construct($return, $authStub->__getLastResponseHTTPCode());
+    
+    if ($this->status){
+      if (property_exists($return, "Results")){
+        // We always want the results property when doing a retrieve to be an array
+        if (is_array($return->Results)){
+          $this->results = $return->Results;
+        } else {
+          $this->results = array($return->Results);
+        }
+      } else {
+        $this->results = array();
+      }
+      if ($return->OverallStatus != "OK" && $return->OverallStatus != "MoreDataAvailable")
+      {
+        $this->status = false;
+        $this->message = $return->OverallStatus;
+      }
+
+      $this->moreResults = false;
+      
+      if ($return->OverallStatus == "MoreDataAvailable") {        
+        $this->moreResults = true;
+      }
+        
+      $this->request_id = $return->RequestID;
+    } 
+  }
 }
 ?>
